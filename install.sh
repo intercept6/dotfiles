e88be3574f2df5f6342a0d416b1fd122586e6670
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-06-07 16:14:43 +0900

    プラグイン・補間のインストールの為にbash-itコマンドを有効にする

--- install.sh
+++ install.sh
@@ -3,18 +3,3 @@
 set -eo pipefail
 
 ~/dotfiles/.bash_it/install.sh --silent
-
-bash-it enable plugin \
-	aws \
-	docker \
-	docker-compose \
-	git \
-	powerline 
-
-bash-it enable completion \
-	awscli \
-	docker-compose \
-	docker \
-	gcloud \
-	git \
-	github-cli 

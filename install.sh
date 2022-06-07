7fc3b22ca3bc92ba391b68378dfa8f9389bc748b
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-06-07 13:10:28 +0900

    chshのパスワードをスキップ

--- install.sh
+++ install.sh
@@ -2,23 +2,12 @@
 
 set -eo pipefail
 
+sudo sed --in-place -e '/auth.*required.*pam_shells.so/s/required/sufficient/g' /etc/pam.d/chsh
+
 if type curl >/dev/null 2>&1; then
 	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-
-	# Download zimfw plugin manager if missing.
-	if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-		curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-				https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-	fi
-
 elif type wget >/dev/null 2>&1; then
 	wget -nv -O - https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-
-	# Download zimfw plugin manager if missing.
-	if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-		mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
-				https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-	fi
 else
 	echo "Please install curl or wget before running this script."	
 fi

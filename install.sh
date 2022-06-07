43ca50bccb956974b5ca3f49b2cb5b067dfc4f2f
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-06-07 11:31:28 +0900

    zimにはプラグインマネージャーが必要

--- install.sh
+++ install.sh
@@ -4,8 +4,21 @@ set -eo pipefail
 
 if type curl >/dev/null 2>&1; then
 	curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
+	# Download zimfw plugin manager if missing.
+	if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+		curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+				https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+	fi
+
 elif type wget >/dev/null 2>&1; then
 	wget -nv -O - https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
+	# Download zimfw plugin manager if missing.
+	if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+		mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh \
+				https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+	fi
 else
 	echo "Please install curl or wget before running this script."	
 fi
